22c19902b4380f65049e4f8d400cdd017efc61b7
Tanjiro Kamado <henri@Kamado4679.local>
2026-05-21 22:24:01 +0300

    fixed typo errors

--- Modern Js Refresher/Refresher.js
+++ Modern Js Refresher/Refresher.js
@@ -11,7 +11,7 @@ console.log(sayHello());
 
 const add = (a, b) =>{// arrow function with multiple arguments - parentheses is mandatory
 
-    a + b;
+   return  a + b;
 
 }
 console.log(add(2, 3));
@@ -44,7 +44,7 @@ console.log(name);
 
 const arr1 = [1, 2, 3];
 const arr2 = [...arr1, 4, 5];
-console,log(arr2);
+console.log(arr2);
 
 // it can also be used to copy objects or combine objects 
 
